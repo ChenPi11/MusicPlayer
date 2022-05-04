62160ae8f7c6a5418ecaf79485f5185fa4cc8b5d
ChenPi11 <102936596+ChenPi11@users.noreply.github.com>
2022-05-04 18:27:11 +0800

    Add files via upload

--- MP.py
+++ MP.py
@@ -1,3 +1,29 @@
+'''                                                      LICENSE
+BSD 2-Clause License
+
+Copyright (c) 2022, ChenPi11
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.'''
 #moudle:pyaudio,numpy
 import wave,pyaudio,os,struct,math,sys,time,platform
 import numpy as np
@@ -86,7 +112,29 @@ def core(f):
         if(i.strip().split(" ")[0]=="#"):
             continue
         else:
-            que.append([e[i.strip().split(" ")[0]],e[i.strip().split(" ")[1]],int(int(i.strip().split(" ")[2]))])
+            l=0
+            r=0
+            try:
+                l=e[i.strip().split(" ")[0]]
+            except:
+                pass
+            try:
+                r=e[i.strip().split(" ")[1]]
+            except:
+                pass
+            try:
+                if(len(i.strip().split(" ")[0])>1):
+                    if(i.strip().split(" ")[0][0]=="r"):
+                        l=int(i.strip().split(" ")[0][1:])
+            except:
+                pass
+            try:
+                if(len(i.strip().split(" ")[1])>1):
+                    if(i.strip().split(" ")[1][0]=="r"):
+                        r=int(i.strip().split(" ")[1][1:])
+            except:
+                pass
+            que.append([l,r,int(int(i.strip().split(" ")[2]))])
             try:
                 echos.append(Echo(i.strip().split(" ")[3],int(int(i.strip().split(" ")[2]))))
             except:
@@ -117,7 +165,7 @@ if(not error):
     wf.setsampwidth(sample_width)
     #mix
     try:
-        core(open(fpath,"r",encoding="utf-8"))
+        core(open(fpath,"r",encoding="GBK"))
         for i in range(len(que)):
             c(que[i][0],que[i][1],wf,que[i][2])
             print(str(i+1)+" "*(len(str(len(que)))-len(str(i+1)))+"/"+str(len(que))+" "+str(int((i+1)/len(que)*100))+"%")
